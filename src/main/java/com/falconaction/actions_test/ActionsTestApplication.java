e84450ffa542aefd7f3335ed58fb1efd50ced0c0
Rohit Patil <rohitpatil1625@gmail.com>
2024-12-18 23:29:10 +0530

    Initial commit for Spring Boot project

---
+++ src/main/java/com/falconaction/actions_test/ActionsTestApplication.java
@@ -0,0 +1,13 @@
+package com.falconaction.actions_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActionsTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/falconaction/actions_test/ActionsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.falconaction.actions_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActionsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
